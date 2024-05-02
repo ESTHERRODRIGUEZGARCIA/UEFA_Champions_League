84d3250366697ff13a187c6a0bf65b3d954e8482
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2024-05-02 14:03:14 +0200

    commit

--- SPARK/info_csv.py
+++ SPARK/info_csv.py
@@ -1,41 +1,38 @@
-import pandas as pd
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
-def create_dataframe_from_csv():
-    # Ruta al archivo CSV
-    file_path = "datos_gradio/datos_semis.csv"
-    
-    # Leer el archivo CSV y crear un DataFrame de pandas
-    df = pd.read_csv(file_path)
-    
-    return df
+class SparkChampionsLeague:
 
+    def __init__(self):
+        self.spark = SparkSession.builder \
+            .appName("Analisis_Champions_Spark") \
+            .getOrCreate()
 
-def create_spark_dataframe_from_csv():
-    # Iniciar SparkSession
-    spark = SparkSession.builder \
-        .appName("Football Data Analysis") \
-        .getOrCreate()
-    
-    # Crear DataFrame de pandas desde CSV
-    pandas_df = create_dataframe_from_csv()
-    
-    # Convertir DataFrame de pandas a DataFrame de Spark
-    spark_df = spark.createDataFrame(pandas_df)
-    
-    return spark_df
+    def stop(self):
+        self.spark.stop()
 
-def main():
-    # Crear DataFrame de Spark
-    spark_df = create_spark_dataframe_from_csv()
-    
-    # Mostrar esquema del DataFrame
-    spark_df.printSchema()
-    
-    # Mostrar los primeros registros del DataFrame
-    spark_df.show()
+    def read_file(self, path):
+        return self.spark.read.csv(path, header=True, inferSchema=True)
+
+    def create_temp_view(self, df, name):
+        df.createOrReplaceTempView(name)
+
+    def sql_query(self, query):
+        return self.spark.sql(query).show()
+
+    def predict(self, df):
+        # Simulación de una función de predicción muy simple
+        team_stats = df.groupBy('Squad').avg('Goals')
+        team_stats.show()
 
 
 if __name__ == "__main__":
-    main()
-    
+    spark_app = SparkChampionsLeague()
+    df = spark_app.read_file("datos_gradio/champions_league.csv")  # Cambia esto al path correcto
+    spark_app.create_temp_view(df, "champions_data")
+    spark_app.sql_query("SELECT * FROM champions_data LIMIT 10")  # Muestra los primeros 10 registros
+    spark_app.predict(df)  # Ejemplo de función de predicción
+    spark_app.stop()
