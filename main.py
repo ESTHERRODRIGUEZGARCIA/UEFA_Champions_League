82b1ef1086e4210a818fbccd8e42536a82f16e53
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2024-05-13 11:28:16 +0200

    organizzamos funciones para el main

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ from modelo_2 import *
 from Parte3 import *
 from RedesNeuronales import *
 from regresion_lineal import *
-from semis import *
+from semis.stats import *
 from model_SPARK import *
 import analisis_arbol_bosque
 import advanced_analysis
 
 def main():
     print("Seleccione el módulo de análisis a ejecutar:")
-    print("1. Análisis Básico")
+    print("1. regresión lineal (Probabilidades Estimadas de Ganar la Champions League 2023-2024 (%))")
     print("2. Análisis Avanzado con LangChain y Llama 3")
     print("3. Regresión Lineal")
+    print("4. Redes Neuronales")
+    print("5. Análisis de Árbol y Bosque")
+    print("6. Análisis de Datos con Spark")
+    print("7. Análisis de Datos con Pandas")
+    print("8. Gráficas de estadísticas de los equipos semifinalistas ")
     choice = input("Ingrese su opción: ")
-    
+    if choice == "1":
+        regresion_lineal()
+    elif choice == "8":
+        semis.stats()
+
     
 
 if __name__ == "__main__":
