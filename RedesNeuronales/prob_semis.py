d2d3899918a671a72d9ea5a808cddf34dbc4beb1
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2024-05-14 14:08:02 +0200

    redes bien

--- RedesNeuronales/prob_semis.py
+++ RedesNeuronales/prob_semis.py
@@ -4,31 +4,32 @@ from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-# Cargar el modelo entrenado
-model = load_model('RedesNeuronales/modelo_entrenado_24_features.h5')
+def probabilidad_semis():
+    # Cargar el modelo entrenado
+    model = load_model('RedesNeuronales/modelo_entrenado_24_features.h5')
 
-# Cargar y preparar los datos como se mostró previamente
-combined_data = pd.read_csv('RedesNeuronales/combined_data.csv')
-scaler = MinMaxScaler()
-features = ['media_goles_ataque', 'tiros_puerta', 'penaltis_favor', 'corners', 'faltas_recibidas', 
-            'fuera_juego', 'goles_recibidos', 'tiros_puerta_recibidos', 'penalti_contra', 'posesion', 
-            'Veces_Campeon', 'Veces_Subcampeon']
-combined_data[features] = scaler.fit_transform(combined_data[features])
+    # Cargar y preparar los datos como se mostró previamente
+    combined_data = pd.read_csv('RedesNeuronales/combined_data.csv')
+    scaler = MinMaxScaler()
+    features = ['media_goles_ataque', 'tiros_puerta', 'penaltis_favor', 'corners', 'faltas_recibidas', 
+                'fuera_juego', 'goles_recibidos', 'tiros_puerta_recibidos', 'penalti_contra', 'posesion', 
+                'Veces_Campeon', 'Veces_Subcampeon']
+    combined_data[features] = scaler.fit_transform(combined_data[features])
 
-# Preparar los datos para los enfrentamientos
-teams_data = combined_data.set_index('equipo')
-rm_bm_data = np.concatenate((teams_data.loc['Real Madrid', features], teams_data.loc['Bayern Munich', features])).reshape(1, -1)
-psg_dortmund_data = np.concatenate((teams_data.loc['Paris Saint-Germain', features], teams_data.loc['Borussia Dortmund', features])).reshape(1, -1)
+    # Preparar los datos para los enfrentamientos
+    teams_data = combined_data.set_index('equipo')
+    rm_bm_data = np.concatenate((teams_data.loc['Real Madrid', features], teams_data.loc['Bayern Munich', features])).reshape(1, -1)
+    psg_dortmund_data = np.concatenate((teams_data.loc['Paris Saint-Germain', features], teams_data.loc['Borussia Dortmund', features])).reshape(1, -1)
 
-# Hacer predicciones
-rm_bm_prob = model.predict(rm_bm_data)
-psg_dortmund_prob = model.predict(psg_dortmund_data)
+    # Hacer predicciones
+    rm_bm_prob = model.predict(rm_bm_data)
+    psg_dortmund_prob = model.predict(psg_dortmund_data)
 
-# Imprimir las probabilidades
-print("Probabilidades de victoria en el enfrentamiento Real Madrid vs Bayern Munich:")
-print(f"Real Madrid: {rm_bm_prob[0][0]*100:.2f}%")
-print(f"Bayern Munich: {rm_bm_prob[0][1]*100:.2f}%\n")
+    # Imprimir las probabilidades
+    print("Probabilidades de victoria en el enfrentamiento Real Madrid vs Bayern Munich:")
+    print(f"Real Madrid: {rm_bm_prob[0][0]*100:.2f}%")
+    print(f"Bayern Munich: {rm_bm_prob[0][1]*100:.2f}%\n")
 
-print("Probabilidades de victoria en el enfrentamiento PSG vs Dortmund:")
-print(f"PSG: {psg_dortmund_prob[0][0]*100:.2f}%")
-print(f"Dortmund: {psg_dortmund_prob[0][1]*100:.2f}%")
+    print("Probabilidades de victoria en el enfrentamiento PSG vs Dortmund:")
+    print(f"PSG: {psg_dortmund_prob[0][0]*100:.2f}%")
+    print(f"Dortmund: {psg_dortmund_prob[0][1]*100:.2f}%")
--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from model_AB.visual import grafica_model_a_b
 from semis.stats import *
 from Parte3.modelo_arima import model_arima
 from Parte3.modelo2_arima import model2_arima
+from RedesNeuronales.prob_semis import *
 
 
 
 def main():
     print("Seleccione el módulo de análisis a ejecutar:")
     print("1. regresión lineal (Probabilidades Estimadas de Ganar la Champions League 2023-2024 (%))")
-    print("2. Análisis Avanzado con LangChain y Llama 3")
-    print("3. Regresión Lineal")
+    print("2. NO FUNCIONA AÚN: Análisis Avanzado con LangChain y Llama 3")
     print("4. Redes Neuronales")
     print("5. Análisis de Árbol y Bosque")
-    print("6. Análisis de Datos con Spark")
+    print("6. NO FUNCIONA AÚN: Análisis de Datos con Spark")
     print("7. Análisis de Datos con series temporales")
     print("8. Gráficas de estadísticas de los equipos semifinalistas ")
     choice = input("Ingrese su opción: ")
     if choice == "1":
+        pass
+    elif choice == "4":
         pass
     elif choice == "5":
         model_a_b()
