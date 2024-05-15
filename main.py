cb3f94aab1f1d19d0081d61103e3fd1b14117a03
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2024-05-15 16:40:02 +0200

    main finalizado

--- main.py
+++ main.py
@@ -13,29 +13,30 @@ from regresion_lineal.parte3 import parte3_analisis
 def main():
     while True:
         print("Seleccione el módulo de análisis a ejecutar:")
-        print("1. regresión lineal (Probabilidades Estimadas de Ganar la Champions League 2023-2024 (%))")
-        print("2. reg lin")
-        print("2. NO FUNCIONA AÚN: Análisis Avanzado con LangChain y Llama 3")
-        print("4. Redes Neuronales")
-        print("5. Análisis de Árbol y Bosque")
-        print("6. NO FUNCIONA AÚN: Análisis de Datos con Spark")
-        print("7. Análisis de Datos con series temporales")
-        print("8. Gráficas de estadísticas de los equipos semifinalistas ")
+        print("1. regresión lineal (Probabilidades Estimadas de Ganar la Champions League 2023-2024 desde octavos (%))")
+        print("2. regresión lineal con un ganador final")
+        print("3. Redes Neuronales")
+        print("4. Análisis de Árbol y Bosque")
+        print("5. Análisis de Datos con series temporales")
+        print("6. Gráficas de estadísticas de los equipos semifinalistas ")
+        print("7. INTENTO (no conseguido): Análisis Avanzado con LangChain y Llama 3")
+        print("8. INTENTO (no conseguido): Análisis de Datos con Spark")
+
         choice = input("Ingrese su opción: ")
         if choice == "1":
             modelo_regresion_lineal_uefa()
         elif choice == "2":
             parte3_analisis()
-        elif choice == "4":
+        elif choice == "3":
             probabilidad_semis()
-        elif choice == "5":
+        elif choice == "4":
             model_a_b()
             grafica_model_a_b()
-        elif choice == "7":
+        elif choice == "5":
             print("Primera opción: Modelo autorregresivo integrado de media móvil (ARIMA)")
             model_arima()
             model2_arima()
-        elif choice == "8":
+        elif choice == "6":
             graficas_semis()
 
         repetir = input("¿Quieres ejecutar otro modelo? Sí:1 No:2 \n")
